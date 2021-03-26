26a9148d812c9cff2b09d746a6ffc2e3da0a8307
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-26 10:11:10 -0700

    add context file

---
+++ SweetAndTreat/Models/SweetAndTreatContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetAndTreat.Models
+{
+  public class SweetAndTreatContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreats { get; set; }
+    public SweetAndTreatContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
