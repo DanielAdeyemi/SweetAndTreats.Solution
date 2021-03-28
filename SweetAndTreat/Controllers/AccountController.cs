bdb3fa58ab8577c96324d064910b9dcfc9d5f2b6
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-28 14:04:38 -0700

    create index route for account controller

---
+++ SweetAndTreat/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using SweetAndTreat.Models;
+using SweetAndTreat.ViewModels;
+
+namespace SweetAndTreat.Controllers
+{
+  public class AccountController : Controller
+  {
+    private readonly SweetAndTreatContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, SweetAndTreatContext db)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
