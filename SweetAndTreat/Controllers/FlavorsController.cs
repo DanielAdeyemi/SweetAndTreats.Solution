d5ccfb91acfbae00b40ae09698b126fa40a15427
DanielAdeyemi <adeyemidany@gmail.com>
2021-03-28 13:05:08 -0700

    create Index controller for flavors

---
+++ SweetAndTreat/Controllers/FlavorsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using SweetAndTreat.Models;
+
+namespace SweetAndTreat.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    private readonly SweetAndTreatContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    public FlavorsController(UserManager<ApplicationUser> userManager, SweetAndTreatContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+  }
+}
