7622d62b777a8c5902df092185d6faf9180e294f
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-01 10:24:07 -0700

    create delete flavor post route

--- SweetAndTreat/Controllers/FlavorsController.cs
+++ SweetAndTreat/Controllers/FlavorsController.cs
@@ -104,5 +104,14 @@ namespace SweetAndTreat.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [HttpPost]
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var joinEntry = _db.FlavorTreats.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreats.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
